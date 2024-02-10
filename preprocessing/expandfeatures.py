a7974f2cc64aafe97cce0bee62a1ceaf43d49608
Selarow <selarowwastaken@gmail.com>
2024-02-10 05:05:51 +0100

    reworked code to write line by line

--- preprocessing/expandfeatures.py
+++ preprocessing/expandfeatures.py
@@ -1,4 +1,4 @@
-import os, sys, psutil
+import os, sys
 from itertools import combinations
 
 
@@ -6,84 +6,80 @@ from itertools import combinations
 class ExpandFeatures():
     NUM_CNF = 5
 
-    def __init__(self, cnfs, file, new_file):
-        self.cnfs = [cnf for cnf in map(int, str(cnfs))]
-        if len(self.cnfs) != self.NUM_CNF:
+    def __init__(self, cnfs, file, file_ex):
+        cnfs = [cnf for cnf in map(int, str(cnfs))]
+
+        if len(cnfs) != self.NUM_CNF:
             raise Exception("Invalid cnfs") 
         
-        self.file = file
-        self.new_file = new_file
-        data = self.parse_file(self.file)
-        data = self.expand_features(data)
-        self.write_file(self.new_file, data)
+        self.cnfs = dict()
+        self.cnfs["x_or_y"] = cnfs[0]
+        self.cnfs["notx_or_y"] = cnfs[1]
+        self.cnfs["x_or_noty"] = cnfs[2]
+        self.cnfs["notx_or_noty"] = cnfs[3]
+        self.cnfs["x_xor_y"] = cnfs[4]
+        self.num_cnfs = sum(x==1 for x in self.cnfs.values())
+        self.expand_features(file, file_ex)
 
 
-    def parse_file(self, file):
-        data = dict()
-        f = open(file, "r")
-        cnt = 0
+    def rawcount(self, file):
+        f = open(file, "rb")
+        lines = 0
+        buf_size = 1024 * 1024
+        read_f = f.raw.read
+        buf = read_f(buf_size)
 
-        for line in f.readlines():
-            data[cnt] = [int(x) for x in line.split(" ")]
-            cnt+=1
-        
-        f.close()
+        while buf:
+            lines += buf.count(b"\n")
+            buf = read_f(buf_size)
 
-        return data
-
-
-    def write_file(self, new_file, data):
-        print("\n")
-        f = open(new_file, "a")
-
-        for i, line in data.items():
-            f.write(" ".join(map(str, line))+"\n")
-            print(f"\rWriting...: {int(((i+1)/len(data))%100 * 100)}%", end=" ", flush=True)
-        
-        f.close()
-        print("Done")
-        file_size = "{:.2f}".format(os.stat(new_file).st_size/1024 ** 2)
-        print(f"New file: {new_file} of size {file_size} MB")
+        return lines
 
 
-    def expand_features(self, data):
-        n, p = len(data), len(data[0])
-        process = psutil.Process()
-        features = [i for i in range(p)][1:]
-        print("Progress = 0%", end="")
+    def expand_features(self, file, file_ex):
+        fex = open(file_ex, "a")
+        num_lines = self.rawcount(file)
 
-        for i, line in data.items():
-            for f1, f2 in combinations(features, 2):
-                if self.cnfs[0] == 1:
-                    x_or_y = line[f1] or line[f2]
-                    line.append(int(x_or_y))
-                
-                if self.cnfs[1] == 1:
-                    notx_or_y = not line[f1] or line[f2]
-                    line.append(int(notx_or_y))
-                
-                if self.cnfs[2] == 1:
-                    x_or_noty = line[f1] or not line[f2]
-                    line.append(int(x_or_noty))
-                
-                if self.cnfs[3] == 1:
-                    notx_or_noty = not line[f1] or not line[f2]
-                    line.append(int(notx_or_noty))
-                
-                if self.cnfs[4] == 1:
-                    x_xor_y = line[f1] ^ line[f2]
-                    line.append(int(x_xor_y))
-            
-            mem = int(process.memory_info().rss/1024 ** 2)
-            print(f"\rProgress: {int(((i+1)/n)%100 * 100)}% | Mem usage: {mem} MB", end="", flush=True)
-        
-        return data
+        with open(file, "r") as f:
+            for cnt, line in enumerate(f):
+                line = [int(i) for i in line.split(" ")]
+
+                if cnt == 0:
+                    line_size = len(line)
+                    features = [i for i in range(line_size)][1:]
+
+                for f1, f2 in combinations(features, 2):
+                    if self.cnfs["x_or_y"]:
+                        x_or_y = line[f1] or line[f2]
+                        line.append(int(x_or_y))
+                    
+                    if self.cnfs["notx_or_y"]:
+                        notx_or_y = not line[f1] or line[f2]
+                        line.append(int(notx_or_y))
+                    
+                    if self.cnfs["x_or_noty"]:
+                        x_or_noty = line[f1] or not line[f2]
+                        line.append(int(x_or_noty))
+                    
+                    if self.cnfs["notx_or_noty"]:
+                        notx_or_noty = not line[f1] or not line[f2]
+                        line.append(int(notx_or_noty))
+                    
+                    if self.cnfs["x_xor_y"]:
+                        x_xor_y = line[f1] ^ line[f2]
+                        line.append(int(x_xor_y))
+
+                fex.write(" ".join(map(str, line))+"\n")
+                file_size = "{:.2f}".format(os.stat(file_ex).st_size/1024 ** 2)
+                print(f"\rProgress: {int(((cnt+1)/num_lines)%100 * 100)}% | File size: {file_size} MiB", end="", flush=True)
+
+        fex.close()
 
 
 
 if __name__ == "__main__":
     cnfs = sys.argv[1]
-    file_name = sys.argv[2]
-    new_file_name = sys.argv[3]
-    ExpandFeatures(cnfs, file_name, new_file_name)
+    file = sys.argv[2]
+    file_ex = sys.argv[3]
+    ExpandFeatures(cnfs, file, file_ex)
     
