05272be7da586246d3313d136628e79e34d77147
Selarow <selarowwastaken@gmail.com>
2024-08-07 17:52:56 +0200

    average length of axps

--- postprocessing/printsol.py
+++ postprocessing/printsol.py
@@ -17,6 +17,10 @@ class PrintSol():
         21: 6,
         25: 7
     }
+    P_ATOM = 1/2
+    P_AND = 1/4
+    P_OR = 3/4
+    P_XOR = 1/2
 
     # dataset should never be an expanded dataset
     def __init__(self, sol, dataset, target):
@@ -33,7 +37,7 @@ class PrintSol():
         with open(dataset) as file:
             line = file.readline()
             features = [int(i) for i in line.split(" ")]
-            features = [i for i in range(len(features))][1:]
+            features = [i-1 for i in range(len(features))][1:]
         
         return features
 
@@ -57,31 +61,87 @@ class PrintSol():
                     l = line.split(":")
                     depth = self.DEPTH[l[0].count(" ")]
 
+                    if "yes" in l[0]:
+                        tag = "yes"
+                    
+                    else:
+                        tag = "no"
+
                     try:
-                        feature = self.LABELS[int(l[-1])]
+                        feature = str(self.LABELS[int(l[-1])])
                     
                     except:
                         feature = str(l[-1])
                     
                     if depth == 0:
-                        root = Node(feature)
-                        nodes[depth] = root
+                        self.root = Node(feature, tag="yes")
+                        nodes[depth] = self.root
                     
                     elif depth > last_depth:
-                        nodes[depth] = Node(feature, parent=nodes[last_depth])
+                        nodes[depth] = Node(feature, parent=nodes[last_depth], tag=tag)
                         last_depth = depth
                     
                     elif depth < last_depth:
-                        nodes[depth] = Node(feature, parent=nodes[depth-1])
+                        nodes[depth] = Node(feature, parent=nodes[depth-1], tag=tag)
                         last_depth = depth
                     
                     else:
-                        nodes[depth] = Node(feature, parent=nodes[depth-1])
+                        nodes[depth] = Node(feature, parent=nodes[depth-1], tag=tag)
 
         #for pre, fill, node in RenderTree(root):
         #    print("%s%s" % (pre, node.name))
         
-        UniqueDotExporter(root).to_picture(target)
+        UniqueDotExporter(self.root).to_picture(target)
+
+
+    def traverse(self, current_node, path, paths):
+        path.append([current_node.tag, current_node.name])
+
+        if not current_node.children:
+            paths.append(list(path))
+        
+        else:
+            for child in current_node.children:
+                self.traverse(child, path, paths)
+        
+        path.pop()
+
+
+    def get_all_paths(self, node):
+        all_paths = []
+        self.traverse(node, [], all_paths)
+
+        return all_paths
+
+
+    def avg_len_axp(self):
+        all_paths = self.get_all_paths(self.root)
+        avg_len_axp = 0
+
+        for path in all_paths:
+            len_axp = 0
+            P_axp = 1
+
+            for type,cond in path[:-1]:
+                if "xor" in cond:
+                    len_axp += 2
+                    P_axp = P_axp * self.P_XOR
+                
+                elif ("or" in cond and type == "yes") or ("and" in cond and type == "no"):
+                    len_axp += 1
+                    P_axp = P_axp * self.P_OR
+                
+                elif ("and" in cond and type == "yes") or ("or" in cond and type == "no"):
+                    len_axp += 2
+                    P_axp = P_axp * self.P_AND
+
+                else:
+                    len_axp += 1
+                    P_axp = P_axp * self.P_ATOM
+            
+            avg_len_axp += len_axp * P_axp
+
+        return avg_len_axp
 
 
 
@@ -90,4 +150,5 @@ if __name__ == "__main__":
     dataset = sys.argv[2]
     target = sys.argv[3]
     Sol = PrintSol(sol, dataset, target)
-    
+    avg = Sol.avg_len_axp()
+    print("AVERAGE AXP LENGTH =", avg)
