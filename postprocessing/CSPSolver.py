58ea264acbdd480ac0f1e17a882da23ff10cb1bc
Selarow <selarowwastaken@gmail.com>
2024-08-20 16:38:37 +0200

    report

--- postprocessing/CSPSolver.py
+++ postprocessing/CSPSolver.py
@@ -9,6 +9,6 @@ problem.addVariable("x104", [0, 1])
 problem.addConstraint(lambda x38: x38 == 1, ["x38"])
 problem.addConstraint(lambda x88: x88 == 1, ["x88"])
 problem.addConstraint(lambda x38, x98: x38 ^ x98, ["x38", "x98"])
-problem.addConstraint(lambda x88, x104: x88 ^ x104, ["x88", "x104"])
+problem.addConstraint(lambda x88, x104: not(x88 ^ x104), ["x88", "x104"])
 
 print(problem.getSolutions())
